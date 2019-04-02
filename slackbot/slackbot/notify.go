883effd85f4bba9d7b8fd4d92c285228484ad97a
Lingyi Wang <lywang07@gmail.com>
2019-04-02 16:22:34 -0700

    add additional info to slack message

--- slackbot/slackbot/notify.go
+++ slackbot/slackbot/notify.go
@@ -13,6 +13,7 @@ import (
 // Notify posts a notification to Slack that the build is complete.
 func Notify(b *cloudbuild.Build, webhook string) {
 	url := fmt.Sprintf("https://console.cloud.google.com/cloud-build/builds/%s", b.Id)
+	server := fmt.Sprintf("https://%s.studio.cd.learningequality.org", b.Substitutions._RELEASE_NAME)
 	var i string
 	switch b.Status {
 	case "SUCCESS":
@@ -25,7 +26,7 @@ func Notify(b *cloudbuild.Build, webhook string) {
 		i = ":question:"
 	}
 	j := fmt.Sprintf(
-		`{"text": "Cloud Build %s complete: %s %s",
+		`{"text": "Cloud Build %s complete: %s %s\nServer <%s> is going to be turned down.\nIf the build fails, please try again later.",
 		    "attachments": [
 				{
 					"fallback": "Open build details at %s",
@@ -37,7 +38,7 @@ func Notify(b *cloudbuild.Build, webhook string) {
 						}
 					]
 				}
-			]}`, b.Id, i, b.Status, url, url)
+			]}`, b.Id, i, b.Status, server, url, url)
 
 	r := strings.NewReader(j)
 	resp, err := http.Post(webhook, "application/json", r)
